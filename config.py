4126b66e900d2ee7d5541df639300895498f52d7
Shreythemasterai <shreyashdubey2007@gmail.com>
2026-07-13 22:13:25 +0530

    Prepare for Streamlit deployment

--- config.py
+++ config.py
@@ -3,7 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-IBM_API_KEY = os.getenv("IBM_API_KEY")
-IBM_PROJECT_ID = os.getenv("IBM_PROJECT_ID")
-IBM_URL = os.getenv("IBM_URL")
-MODEL_ID = os.getenv("IBM_MODEL_ID")
+try:
+    import streamlit as st
+
+    IBM_API_KEY = st.secrets["IBM_API_KEY"]
+    IBM_PROJECT_ID = st.secrets["IBM_PROJECT_ID"]
+    IBM_URL = st.secrets["IBM_URL"]
+    MODEL_ID = st.secrets["IBM_MODEL_ID"]
+
+except Exception:
+    IBM_API_KEY = os.getenv("IBM_API_KEY")
+    IBM_PROJECT_ID = os.getenv("IBM_PROJECT_ID")
+    IBM_URL = os.getenv("IBM_URL")
+    MODEL_ID = os.getenv("IBM_MODEL_ID")
